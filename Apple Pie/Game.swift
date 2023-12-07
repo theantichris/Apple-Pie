1a0ab32fbf0be9ef8d0a5f4ab59e75d5a7f9a7d2
Christopher Lamm <christopher.lamm@companycam.com>
2023-12-07 09:08:42 -0500

    added game state, logic for starting a round, function to update UI

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Christopher Lamm on 12/7/23.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var listOfWords = ["buccaneer", "swift", "glorious", "incadescent", "bug", "program"]
+    let incorrectMovesAllowed = 7
+    var totalWins = 0
+    var totalLosses = 0
+    var currentGame: Game!
+    
+    @IBOutlet var treeImageView: UIImageView!
+    @IBOutlet var correctWordLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        newRound()
+    }
+    
+    func updateUI() {
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
     }
 
+    func newRound() {
+        let newWord = listOfWords.removeFirst()
+        
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
+        
+        updateUI()
+    }
 
+    @IBAction func letterButtonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
 }
 
