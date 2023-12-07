c277d6598e2d798f1f5bf630ee025d4023f61817
Christopher Lamm <christopher.lamm@companycam.com>
2023-12-07 10:33:33 -0500

    added logic to display the correctly guessed letters

--- Apple Pie/Game.swift
+++ Apple Pie/Game.swift
@@ -12,6 +12,20 @@ struct Game {
     var incorrectMovesRemaining: Int
     var guessedLetters: [Character]
     
+    var formattedWord: String {
+        var guessedWord = ""
+        
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        
+        return guessedWord
+    }
+    
     mutating func playerGuessed(letter: Character) {
         guessedLetters.append(letter)
         
--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -26,6 +26,13 @@ class ViewController: UIViewController {
     }
     
     func updateUI() {
+        var letters = [String]()
+        for letter in currentGame.formattedWord {
+            letters.append(String(letter))
+        }
+        let wordWithSpacing = letters.joined(separator: " ")
+        correctWordLabel.text = wordWithSpacing
+        
         scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
         
         treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
